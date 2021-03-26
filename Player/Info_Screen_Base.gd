98817e330cae46c2a7c7ed05b5d50ad5d555cf18
clootch <tpwmustang@gmail.com>
2021-03-26 16:05:45 -0500

    Updated Player Backpack

---
+++ Player/Info_Screen_Base.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+onready var node = get_node(".")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Roster_Open_Roster():
+	
+	var children = node.get_children()
+	for child in children:
+		child.visible = !child.visible
+	node.visible = !node.visible
+
+
+func _on_Back_back_button_pressed():
+	node.visible = !node.visible
+	var children = node.get_children()
+	for child in children:
+		child.visible = !child.visible
+		
---
+++ Player/Roster_Screen.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Roster_Open_Roster():
+	self.visible = !self.visible
+
+
+func _on_Back_back_button_pressed():
+	self.visible = !self.visible
