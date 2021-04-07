2baa6a3228f21f072030b9f51e2f6438e60a42fa
clootch <tpwmustang@gmail.com>
2021-04-07 12:50:40 -0500

    Menuing Updates

--- Rooms/Back.gd
+++ Rooms/Back.gd
@@ -4,17 +4,16 @@ extends Button
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-signal back_button_pressed
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func _pressed():
-	emit_signal("back_button_pressed")
 	var parent = get_node("..")
 	parent.visible = !parent.visible
+	var children = parent.get_parent().get_children()
+	for child in children:
+		if child.name == "Menu":
+			child.visible = !child.visible
