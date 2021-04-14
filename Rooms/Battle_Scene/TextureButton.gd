a684003fe73a23ba24e184caf9b6df5755fc1d70
clootch <tpwmustang@gmail.com>
2021-04-14 13:00:06 -0500

    Player Backpack Updates

--- Rooms/Battle_Scene/TextureButton.gd
+++ Rooms/Battle_Scene/TextureButton.gd
@@ -8,11 +8,11 @@ extends TextureButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var thing = load(Globals.prof_current[Globals.temp_player_roster[0]]["sprite"])
-	
-	self.texture_normal = load(Globals.prof_current[Globals.temp_player_roster[0]]["sprite"])
-	self.texture_hover = load(Globals.prof_current[Globals.temp_player_roster[1]]["sprite"])
-	self.texture_pressed = load(Globals.prof_current[Globals.temp_player_roster[2]]["sprite"])
+	#var thing = load(Globals.prof_current[Globals.temp_player_roster[0]]["sprite"])
+	var keys = Globals.prof_current.keys()
+	self.texture_normal = load(Globals.prof_current[keys[0]]["sprite"])
+	self.texture_hover = load(Globals.prof_current[keys[1]]["sprite"])
+	self.texture_pressed = load(Globals.prof_current[keys[2]]["sprite"])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
