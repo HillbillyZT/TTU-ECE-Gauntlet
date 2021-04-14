504f9b89030e374b55e122a74e117c1626409a53
HillbillyZT <chris30257@gmail.com>
2021-04-13 22:01:35 -0500

    room prepwork

--- Room_Transition.gd
+++ Room_Transition.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
+export var portal_exit_pos = Vector2(0,0)
+
+
 func _physics_process(delta):
 	var bodies = get_overlapping_bodies()
 	for body in bodies:
 		if body.name == "Player":
 			#get_tree().change_scene("res://Rooms/Test_Room.tscn") #no persistence
+			#Globals.new_player_pos = body.position
 			SceneSwitcher.change_scene_instanced("res://Rooms/Test_Room.tscn") #wrong, impersistent
+			#SceneSwitcher.change_scene(SceneSwitcher.rooms[0])
 	
