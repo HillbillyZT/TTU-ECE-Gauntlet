636aa053290c5848e749e26dd0b4633d76c8f3cf
clootch <68715725+clootch@users.noreply.github.com>
2021-04-22 22:40:02 -0500

    Minor Update to Player backpack
    It now uses a dictionary lol

--- Player/Player_Backpack.gd
+++ Player/Player_Backpack.gd
@@ -4,7 +4,8 @@ extends Node2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var items =[]
+#var items =[]
+var items = {}
 signal update
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,5 +18,8 @@ func _ready():
 
 
 func _on_Confirm_buy(item):
-	items.append(item)
+	if items.has(item):
+		items[item]["Quantity"] += 1
+	else:
+		items[item] = {"name":item,"Quantity":1}
 	emit_signal("update",item)
