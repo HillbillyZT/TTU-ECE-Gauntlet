1cf16390fbe650d30a067cd4c9d65626e9800f91
HillbillyZT <chris30257@gmail.com>
2021-05-02 23:59:38 -0500

    Revert "I'm refactoring your shit Hunter I just can't take it anymore"
    This reverts commit c9339589d47e38d3a4c7743851ec82d60a10f164.

--- Rooms/Battle_Scene/Prof2.gd
+++ Rooms/Battle_Scene/Prof2.gd
@@ -2,17 +2,15 @@ extends Button
 
 signal change_displayed
 signal change_cur_prof
-onready var prof_animated := $Prof1_Animated
-
+onready var prof_animated := $Prof2_Animated
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var keys = Globals.temp_player_roster.keys()
-	if keys.size() <= 0:
-		print("um... This shouldn't be possible...")
+	if keys.size() <= 1:
 		self.visible = false
 		prof_animated.visible = false
 	else:
-		self.text = keys[0]
+		self.text = keys[1]
 		if self.text in prof_animated.frames.get_animation_names():
 			prof_animated.play(self.text)
 
