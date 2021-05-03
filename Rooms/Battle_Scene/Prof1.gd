c9339589d47e38d3a4c7743851ec82d60a10f164
HillbillyZT <chris30257@gmail.com>
2021-05-02 23:56:32 -0500

    I'm refactoring your shit Hunter I just can't take it anymore
    You're welcome, Cole.

--- Rooms/Battle_Scene/Prof1.gd
+++ Rooms/Battle_Scene/Prof1.gd
@@ -2,20 +2,31 @@ extends Button
 
 signal change_displayed
 signal change_cur_prof
-onready var prof_animated := $Prof1_Animated
+export var index = -1
+var prof_animated
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	match(index):
+		0: prof_animated = $Prof1_Animated
+		1: prof_animated = $Prof2_Animated
+		2: prof_animated = $Prof3_Animated
+		3: prof_animated = $Prof4_Animated
+		4: prof_animated = $Prof5_Animated
+		5: prof_animated = $Prof6_Animated
+		
+	
 	var keys = Globals.temp_player_roster.keys()
-	if keys.size() <= 0:
+	if keys.size() <= index:
 		print("um... This shouldn't be possible...")
 		self.visible = false
 		prof_animated.visible = false
 	else:
-		self.text = keys[0]
+		self.text = keys[index]
 		if self.text in prof_animated.frames.get_animation_names():
 			prof_animated.play(self.text)
 
 func _pressed():
 	emit_signal("change_displayed",Globals.prof_current[self.text]["sprite"])
+	
 	emit_signal("change_cur_prof",self.text)
