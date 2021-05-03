220b0d2055c1d3e83a6a95ea18b6380bb44079b1
clootch <tpwmustang@gmail.com>
2021-05-03 13:34:07 -0500

    Revert "YOu got the homie up front lol"
    This reverts commit ac41cd7b5a69533e65a3600679083913efa8d7ba, reversing
    changes made to fde8db758ce1e644fa21f121344b758867ee3e50.

--- Player/Info_Screen_Base.gd
+++ Player/Info_Screen_Base.gd
@@ -6,7 +6,6 @@ extends Control
 # var b = "text"
 onready var node = get_node(".")
 onready var gpa_lbl = get_node("GPA_Display")
-onready var doge_lbl = get_node("Doge")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	gpa_lbl.set_text("GPA  - %.2f" % (Globals.player_GPA))
-	doge_lbl.set_text("Doge - %d" % (Globals.doge))
+	gpa_lbl.set_text("GPA - %.2f" % (Globals.player_GPA))
 
 func _on_Roster_Open_Roster():
 	node.visible = !node.visible
